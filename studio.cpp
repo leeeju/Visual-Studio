44ba1cb79e3e3647350dee9a40a84c6c3f46d2be
이주현 <84003327+leeeju@users.noreply.github.com>
2021-05-28 17:35:03 +0900

    비쥬얼 스튜디오 실습 내용

---
+++ studio.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+#include <string>
+#include <ios>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+	cout << "동무 이름이 뭐입네까? : ";
+	string name;
+	cin >> name;
+
+	cout << "내이름은  : " + name << endl;
+	cout << "점수는 : ";
+
+	int midterm, finalterm;
+	cin >> midterm >> finalterm;
+
+	cout << " 니 공부 점수  ";
+	std::vector<double> homework;
+
+	double hw;
+	while (cin >> hw) {
+		homework.push_back(hw);
+	}
+	if (homework.size() == 0) {
+		cout << " " << endl;
+		return 1;
+	}
+	int mid;
+	sort (homework.begin(), homework.end());
+
+	if (homework.size() % 2 == 1)
+		mid = homework[homework.size() / 2];
+	else
+		mid = (homework[homework.size() / 2] + homework[homework.size()] / 2 - 1);
+
+	std::streamsize prec = cout.precision();
+	cout << std::setprecision(3);
+	cout << "오늘의 점수는~~ : " << 0.2 * midterm + 0.4 * finalterm + 0.4 * mid << endl;
+	cout << std::setprecision(prec);
+
+	return 0;
+}
